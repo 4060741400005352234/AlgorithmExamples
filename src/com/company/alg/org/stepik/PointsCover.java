a5a2a3942bebb0538a78657940a050410d9d84b1
Yevgeniy_Popov <yevgeniy_popov@epam.com>
2018-12-28 14:43:20 +0300

    Comment 6

--- src/com/company/alg/org/stepik/PointsCover.java
+++ src/com/company/alg/org/stepik/PointsCover.java
@@ -12,6 +12,8 @@ public class PointsCover {
     }
 
     // O(nLogN) = T(sort) + O(n)
+    //
+    //
     private static int coverPoints(int[] points, int lineLength) {
         Arrays.sort(points);
         int min = 1;
